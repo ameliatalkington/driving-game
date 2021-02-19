c355e24fbf227193c9fa0340755654b1f9831cfd
ameliatalkington <amrtalk@stanford.edu>
2021-02-19 11:15:11 -0700

    in progress

--- main.js
+++ main.js
@@ -14,24 +14,39 @@ var carData = {
 };
 
 function moveCar() {
-  translate = 'translate(' + carData.location.x + 'px, ' + carData.location.y + 'px)';
-  carData.location.x += 7;
-  $car.style.transform = translate;
+  if (carData.direction === 'east') {
+    translate = 'translate(' + carData.location.x + 'px, ' + carData.location.y + 'px)';
+    carData.location.x += 7;
+    $car.style.transform = translate;
+  } else if (carData.direction === 'west') {
+    translate = 'translate(' + carData.location.x + 'px, ' + carData.location.y + 'px)';
+    carData.location.x -= 7;
+    $car.style.transform = translate;
+  } else if (carData.direction === 'north') {
+    translate = 'translate(' + carData.location.x + 'px, ' + carData.location.y + 'px)';
+    carData.location.y += 7;
+    $car.style.transform = translate;
+  } else if (carData.direction === 'south') {
+    translate = 'translate(' + carData.location.x + 'px, ' + carData.location.y + 'px)';
+    carData.location.y -= 7;
+    $car.style.transform = translate;
+  }
+
 }
 
 document.addEventListener('keydown', event => {
   if (event.key === 'ArrowRight') {
+    carData.direction = 'east';
     $car.style.transform = 'rotate(0deg)';
-    carData[0].direction = 'east';
   } else if (event.key === 'ArrowLeft') {
+    carData.direction = 'west';
     $car.style.transform = 'rotate(-180deg)';
-    carData[0].direction = 'west';
   } else if (event.key === 'ArrowUp') {
+    carData.direction = 'north';
     $car.style.transform = 'rotate(-90deg)';
-    carData[0].direction = 'north';
   } else if (event.key === 'ArrowDown') {
+    carData.direction = 'south';
     $car.style.transform = 'rotate(90deg)';
-    carData[0].direction = 'south';
   } else if (event.key === ' ') {
     if (spaceCounter % 2 === 0) {
       interval = setInterval(moveCar, 16);
